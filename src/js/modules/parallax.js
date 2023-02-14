edf6838f9c3250b266e8c94f6ac55062617d1823
Blooming Burden <flowersoverskulls96@gmail.com>
2023-02-14 14:14:59 +0300

    Finished parallax effect v1.0.0 beta

--- src/js/modules/parallax.js
+++ src/js/modules/parallax.js
@@ -5,31 +5,51 @@ let posY = 0;
 let lastPosX = 0;
 let lastPosY = 0;
 
+const coords = {
+    left: pictureList.getBoundingClientRect().left,
+    top: pictureList.getBoundingClientRect().top,
+    bottom: pictureList.getBoundingClientRect().bottom - window.innerHeight,
+    right: pictureList.getBoundingClientRect().right - window.innerWidth,
+};
+
 const SPEED = 0.005;
 
+const checkBorder = () => {
+    if (coords.left >= 0) lastPosX = 0;
+    if (coords.top >= 0) lastPosY = 140;
+    if (coords.bottom <= 0) lastPosY = 200;
+    if (coords.right <= 0) lastPosX = -100;
+}
+
 const moveParallax = () => {
+    coords.left = pictureList.getBoundingClientRect().left;
+    coords.top = pictureList.getBoundingClientRect().top;
+    coords.right = pictureList.getBoundingClientRect().right - window.innerWidth;
+    coords.bottom = pictureList.getBoundingClientRect().bottom - window.innerHeight;
+
     let shiftX = posX - lastPosX;
     let shiftY = posY - lastPosY;
 
     lastPosX = lastPosX + (shiftX * SPEED);
     lastPosY = lastPosY + (shiftY * SPEED);
 
-    pictureList.style.cssText = `transform: translate(${lastPosX}px, ${lastPosY}px) scale(9)`;
+    checkBorder();
 
+    pictureList.style.cssText = `transform: translate(${lastPosX}px, ${lastPosY}px) scale(9)`;
+    
     requestAnimationFrame(moveParallax);
 }
 moveParallax();
 
 const startParallax = (evt) => {
-    const coords = {
-        left: Math.abs(pictureList.getBoundingClientRect().left),
-        top: Math.abs(pictureList.getBoundingClientRect().top),
-    };
+    const left = Math.abs(coords.left)
+    const top = Math.abs(coords.top);
 
-    const blockX =  coords.left + window.innerWidth / 2;
-    const blockY = coords.top + window.innerHeight / 2;
-    posX = evt.pageX + coords.left - blockX;
-    posY = evt.pageY +  coords.top - blockY;
+    const blockX = left + window.innerWidth / 2;
+    const blockY = top + window.innerHeight / 2;
+
+    posX = evt.pageX + left - blockX;
+    posY = evt.pageY + top - blockY;
 }
 
 const checkWindowWidth = () => {
@@ -38,7 +58,7 @@ const checkWindowWidth = () => {
     } else {
         picture.removeEventListener('pointermove', startParallax);
     }
-}
+};
 
 const parallax = () => {
     picture.ondragstart = () => false;
@@ -46,7 +66,6 @@ const parallax = () => {
     window.addEventListener('resize', () => {
         checkWindowWidth();
     })
-
     checkWindowWidth();
 };
 
